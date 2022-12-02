4bb2e670321919533f87f3252cbdca9833cb872e
ominiverdi <info@lorenzobecchi.com>
2022-12-02 22:03:57 +0100

    publishing all pages from config

--- scripts/publisher.js
+++ scripts/publisher.js
@@ -6,12 +6,24 @@ let rawdata = fs.readFileSync('../config.json');
 let config = JSON.parse(rawdata);
 
 //generate home page
+page_writer({ title: config.title, content: config.home_content, filename: 'index.html' })
 
-//load template
-const template = fs.readFileSync("../templates/page.html");
-//load into parser
-const $ = cheerio.load(template);
-$('title').html(config.title)
-$('body').append(config.home_content)
-//save file
-fs.writeFileSync('../www/index.html', $.html());
+//generate pages
+config.pages.forEach((page)=>{
+    console.log(page)
+    page_writer(page)
+})
+
+
+function page_writer(page) {
+    //load template
+    const template = fs.readFileSync("../templates/page.html");
+    //load into parser
+    const $ = cheerio.load(template);
+    $('title').html(page.title)
+    $('body').append(page.content)
+    //save file
+    let pagename=(page.filename)?page.filename:page.title.toLowerCase().replace(' ','_')+'.html';
+
+    fs.writeFileSync('../www/' + pagename, $.html());
+}
