c12d5a2bc3d38c77a34e4be9558adbe2c2a6a8a7
ominiverdi <info@lorenzobecchi.com>
2022-12-03 19:02:36 +0100

    cleaning up

--- scripts/image_optimizer.js
+++ scripts/image_optimizer.js
@@ -3,27 +3,28 @@ const fsp = require('fs').promises;
 var sharp = require('sharp');
 
 //define image max width
-const max_width=1000;
+const max_width = 1000;
 
 //optimize and move images
 fs.readdir('../images', (error, fileNames) => {
     if (error) throw error;
     fileNames.forEach(filename => {
-        // console.log(filename)
         process_image(filename)
     })
 })
 
 async function process_image(filename) {
-    const scaleByHalf = await sharp('../images/' + filename)
+    //resize image
+    const resized = await sharp('../images/' + filename)
         .metadata()
         .then(({ width }) => {
-            console.log(filename,width)
-            if(width>max_width) width=max_width;
+            console.log(filename, width)
+            if (width > max_width) width = max_width;
             return sharp('../images/' + filename)
-            .resize(width)
-            .toBuffer()
+                .resize(width)
+                .toBuffer()
         }
         );
-    fsp.writeFile('../www/img/' + filename, scaleByHalf);
-  }
+    //save to output
+    fsp.writeFile('../www/img/' + filename, resized);
+}
